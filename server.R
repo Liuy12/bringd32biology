f756c638bea0dbc1222630bfeff25db098d875c9
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2015-08-11 09:38:52 -0500

    'First quick and dirty play of d3 heatmap'

--- server.R
+++
@@ -1,8 +0,0 @@
-diabetesRisk <- function(glucose) glucose/200
-shinyServer(
-  function(input,output){
-    output$inputValue <- renderPrint({input$glucose})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
-  }
-)
-
