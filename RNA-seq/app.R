2d1f60d0f6dbda8b21f5ee2973797fabb67eeacd
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2015-08-12 00:26:42 -0500

    Adding density plot

--- RNA-seq/app.R
+++ RNA-seq/app.R
@@ -2,6 +2,8 @@ library(data.table)
 library(shiny)
 library(d3heatmap)
 library(networkD3)
+library(DT)
+library(metricsgraphics)
 
 ui <- fluidPage(
   titlePanel("RNA-seq"),
@@ -14,8 +16,9 @@ ui <- fluidPage(
     ),
     mainPanel(
       tabsetPanel(
+        tabPanel("Table", dataTableOutput("Table")),
         tabPanel("Heatmap", d3heatmapOutput('Heatmap')), 
-        tabPanel("Density", plotOutput("Density")), 
+        tabPanel("Density", metricsgraphicsOutput("Density")), 
         tabPanel("Boxplot", plotOutput("Boxplot")),
         tabPanel("SampleCorrelation", plotOutput("SampleCorrelation")),
         tabPanel("forceNetworkSample", forceNetworkOutput("forceNetworkSample")),
@@ -33,12 +36,41 @@ server <- function(input, output) {
       return(NULL)
     fread(inFile$datapath, data.table=F)
   })
+  output$Table <- renderDataTable({
+    if (is.null(input$file1))
+      return(NULL)
+    datatable(dataMat(), options = list(pageLength = 5))
+  })
   output$Heatmap <- renderD3heatmap({
     if (is.null(input$file1))
       return(NULL)
     d3heatmap(dataMat(), scale="row", colors=colorRampPalette(c("blue","white","red"))(1000))
   })
-  # generate network using networkD3
+  output$Density <- renderMetricsgraphics({
+    if (is.null(input$file1))
+      return(NULL)
+    dataMat <- dataMat()
+    denStat <- density(dataMat[,1], from = min(dataMat), to = max(dataMat))
+    denStat <- data.frame(x = denStat$x,
+                          y = denStat$y)
+    denStat1 <- sapply(2:ncol(dataMat), function(i){
+      preset <- density(dataMat[,i], from = min(dataMat), to = max(dataMat))
+      preset$y
+    })
+    denStat <- cbind(denStat, denStat1)
+    colnames(denStat) <- c('Exprs', paste('S',1:ncol(dataMat),sep = ''))
+    denPlot <- mjs_plot(denStat, Exprs, S1, decimals = 6) %>% 
+      mjs_line() %>%
+      mjs_add_line(S2) %>%
+      mjs_add_line(S3) %>%
+      mjs_add_line(S4) %>%
+      mjs_add_line(S5) %>%
+      mjs_add_line(S6)
+    denPlot <- denPlot %>% 
+      mjs_labs(x_label = 'Expression', y_label = 'Density') %>%
+      mjs_add_legend(legend=colnames(dataMat))
+    denPlot
+  })
   output$forceNetworkSample <- renderForceNetwork({
     if (is.null(input$file1))
       return(NULL)
