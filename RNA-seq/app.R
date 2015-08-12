c1c0c722b22a2556b3e41aa6951ff1b9de686c13
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2015-08-12 17:19:34 -0500

    Add webtitles and use rcharts for density plot

--- RNA-seq/app.R
+++ RNA-seq/app.R
@@ -3,10 +3,14 @@ library(shiny)
 library(d3heatmap)
 library(networkD3)
 library(DT)
-library(metricsgraphics)
+library(rCharts)
+library(shinythemes)
 
 ui <- fluidPage(
-  titlePanel("RNA-seq"),
+  theme = shinytheme("flatly"),
+  fluidRow(column(12, tags$header(strong(HTML("<p>RNA-seq widget: Bring <span style='color: red;'>D3</span> visualization to RNA-seq!")), style = "font-size: 50px; 
+                                  background-color: #F0FFFF;"))),
+  tags$hr(),
   sidebarLayout(
     sidebarPanel(
       fileInput('file1', 'Choose CSV/TXT File',
@@ -18,10 +22,19 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("Table", dataTableOutput("Table")),
         tabPanel("Heatmap", d3heatmapOutput('Heatmap')), 
-        tabPanel("Density", metricsgraphicsOutput("Density")), 
+        tabPanel("Density", showOutput("Density", "nvd3")), 
+        tabPanel("Scatter Plot", sidebarLayout(
+          sidebarPanel(
+            textInput("text1", label = h5("Enter first sample name"), value = "Enter text..."),
+            verbatimTextOutput("value1"),
+            tags$hr(),
+            textInput("text2", label = h5("Enter second sample name"), value = "Enter text..."),
+            verbatimTextOutput("value2")
+          ),
+          mainPanel(showOutput("ScatterPlot", "polycharts")))
+        ),
         tabPanel("Boxplot", plotOutput("Boxplot")),
-        tabPanel("SampleCorrelation", plotOutput("SampleCorrelation")),
-        tabPanel("forceNetworkSample", forceNetworkOutput("forceNetworkSample")),
+        tabPanel("Principal Component", forceNetworkOutput("PrincipalComponent")),
         tabPanel("forceNetworkGene", forceNetworkOutput("forceNetworkGene"))
         )
     )
@@ -36,42 +49,56 @@ server <- function(input, output) {
       return(NULL)
     fread(inFile$datapath, data.table=F)
   })
+  
   output$Table <- renderDataTable({
     if (is.null(input$file1))
       return(NULL)
+    colnames(dataMat()) <- paste('S', 1:ncol(dataMat()), sep='')
     datatable(dataMat(), options = list(pageLength = 5))
   })
+  
   output$Heatmap <- renderD3heatmap({
     if (is.null(input$file1))
       return(NULL)
+    colnames(dataMat()) <- paste('S', 1:ncol(dataMat()), sep='')
     d3heatmap(dataMat(), scale="row", colors=colorRampPalette(c("blue","white","red"))(1000))
   })
-  output$Density <- renderMetricsgraphics({
+  
+  output$Density <- renderChart({
     if (is.null(input$file1))
       return(NULL)
     dataMat <- dataMat()
     denStat <- density(dataMat[,1], from = min(dataMat), to = max(dataMat))
     denStat <- data.frame(x = denStat$x,
                           y = denStat$y)
-    denStat1 <- sapply(2:ncol(dataMat), function(i){
+    denStat1 <- sapply(1:ncol(dataMat), function(i){
       preset <- density(dataMat[,i], from = min(dataMat), to = max(dataMat))
       preset$y
     })
-    denStat <- cbind(denStat, denStat1)
-    colnames(denStat) <- c('Exprs', paste('S',1:ncol(dataMat),sep = ''))
-    denPlot <- mjs_plot(denStat, Exprs, S1, decimals = 6) %>% 
-      mjs_line() %>%
-      mjs_add_line(S2) %>%
-      mjs_add_line(S3) %>%
-      mjs_add_line(S4) %>%
-      mjs_add_line(S5) %>%
-      mjs_add_line(S6)
-    denPlot <- denPlot %>% 
-      mjs_labs(x_label = 'Expression', y_label = 'Density') %>%
-      mjs_add_legend(legend=colnames(dataMat))
-    denPlot
+    colnames(denStat1) <- c(paste('S',1:ncol(dataMat),sep = ''))
+    denStat1 <- stack(as.data.frame(denStat1))
+    denStat <- cbind(rep(denStat[,1], times=ncol(dataMat)), denStat1)
+    colnames(denStat) <- c('Exprs', 'Density', 'ind')
+    np <- nPlot(Density ~ Exprs, group = 'ind', data = denStat, type = 'lineChart')
+    np$addParams(dom = "Density")
+    return(np)
   })
-  output$forceNetworkSample <- renderForceNetwork({
+  
+  output$value1 <- renderPrint({input$text1})
+  
+  output$value2 <- renderPrint({input$text2})
+  
+  output$ScatterPlot <- renderChart({
+    if (is.null(input$file1))
+      return(NULL)
+    dataMat <- dataMat()
+    colnames(dataMat) <- paste('S', 1:ncol(dataMat), sep='')
+    rp <- rPlot(input$text1, input$text2, data = dataMat, type = 'point')
+    rp$addParams(dom = "ScatterPlot")
+    return(rp)
+  })
+  
+  output$PrincipalComponent <- renderForceNetwork({
     if (is.null(input$file1))
       return(NULL)
     dataMat <- dataMat()
