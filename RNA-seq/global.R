fb969026a70543a0a48624c5f0837bdb54eeb434
Yuanhang Liu <yuanhangliu.ok@gmail.com>
2015-10-15 18:14:25 -0500

    Add spikein option and function to identify highly variable genes

--- RNA-seq/global.R
+++ RNA-seq/global.R
@@ -22,6 +22,11 @@ library(limma)
 library(rga)
 library(threejs)
 library(scde)
+# library(BASiCS) too slow
+library(SAMstrt)
+library(RUVSeq)
+library(statmod)
+
 
 #Emails
 # emailList<-readLines(emailFile)
@@ -45,10 +50,15 @@ library(scde)
 # }
 
 XBSeq_pfun <- 
-  function(counts, bgcounts, group, disp_method, sharing_mode, fit_type, paraMethod){
+  function(counts, bgcounts, group, disp_method, sharing_mode, fit_type, paraMethod, spikeins){
     XB <- XBSeqDataSet(counts, bgcounts, group)
     XB <- estimateRealCount(XB)
-    XB <- estimateSizeFactors(XB)
+    if(!is.null(spikeins)){
+      counts <- rbind(spikeins, counts(XB))
+      sizeFactors(XB) <- DESeq2::estimateSizeFactorsForMatrix(counts, controlGenes = 1:nrow(spikeins))
+    }
+    else
+      XB <- estimateSizeFactors(XB)
     XB <- estimateSCV(XB, method = disp_method, sharingMode = sharing_mode, fitType = fit_type)
     Teststas <- XBSeqTest( XB, levels(group)[1L], levels(group)[2L], method =paraMethod)
     Disp <- XBSeq::fitInfo(XB)
@@ -68,14 +78,20 @@ XBSeq_pfun <-
 DESeq2_pfun <-
   function(counts, group, design = NULL, cookcutoff, fittype, test, spikeins)
   {   
+    if(!is.null(spikeins))
+      counts <- rbind(spikeins, counts)
     colData <- data.frame(group)
     dse <- DESeqDataSetFromMatrix(countData = counts, colData = colData, design = ~ group)
-    dse <- estimateSizeFactors()
+    if(!is.null(spikeins))
+      dse <- estimateSizeFactors(dse, controlGenes = 1:nrow(spikeins))
+    else
+      dse <- estimateSizeFactors(dse)
+    dse <- estimateDispersions(dse, fitType = fittype)
     colData(dse)$group <- as.factor(colData(dse)$group)
     if(test == 'LRT')
-      dse <- DESeq(dse, test = 'LRT', fitType = fittype, reduced = ~1)
+      dse <- nbinomLRT(dse, reduced = ~1)
     else
-      dse <- DESeq(dse, test = 'Wald', fitType = fittype)
+      dse <- nbinomWaldTest(dse)
     if(cookcutoff == 'on')
       res <- results(dse)
     else
@@ -89,11 +105,15 @@ DESeq2_pfun <-
   }
 
 DESeq_pfun <-
-  function(counts, group, disp_method, sharing_mode, fit_type)
+  function(counts, group, disp_method, sharing_mode, fit_type, spikeins)
   {   
-    ## implement DESeq using pooled method to estimate dispersion ##
     de <- newCountDataSet(counts, group)
-    de <- estimateSizeFactors(de)
+    if(!is.null(spikeins)){
+      counts <- rbind(spikeins, counts)
+      sizeFactors(de) <- DESeq2::estimateSizeFactorsForMatrix(counts, controlGenes = 1:nrow(spikeins))
+    }
+    else
+      de <- estimateSizeFactors(de)
     de <- DESeq::estimateDispersions(de, method = disp_method, sharingMode = sharing_mode, fitType = fit_type)
     res <- nbinomTest(de, levels(group)[1], levels(group)[2])
     Disp <- DESeq::fitInfo(de)
@@ -111,10 +131,16 @@ DESeq_pfun <-
   }
 
 edgeR.pfun <-
-  function(counts, group, design = NULL)
+  function(counts, group, design = NULL, spikeins)
   {
     ## edgeR standard pipeline ##
-    library(edgeR)
+    if(!is.null(spikeins)){
+      counts1 <- rbind(spikeins, counts)
+      set <- RUVg(as.matrix(counts1), 1:nrow(spikeins), k=1)
+      Phenod <- data.frame(x = group, weight = set$W)
+      rownames(Phenod) <- colnames(counts)
+      design <- model.matrix(~x + W_1, data = Phenod)
+    }
     d <- DGEList(counts = counts, group = group)
     d <- calcNormFactors(d)
     d <- estimateGLMCommonDisp(d, design = design)
@@ -143,9 +169,15 @@ edgeR.pfun <-
   }
 
 edgeR_robust.pfun <-
-  function(counts, group, design = NULL)
+  function(counts, group, design = NULL, spikeins)
   {   
-    ## edgeR-robsut pipeline ##
+    if(!is.null(spikeins)){
+      counts1 <- rbind(spikeins, counts)
+      set <- RUVg(as.matrix(counts1), 1:nrow(spikeins), k=1)
+      Phenod <- data.frame(x = group, weight = set$W)
+      rownames(Phenod) <- colnames(counts)
+      design <- model.matrix(~x + W_1, data = Phenod)
+    }
     d <- DGEList(counts = counts, group = group)
     d <- calcNormFactors(d)
     dw <- estimateGLMRobustDisp(d,design=design)
@@ -172,10 +204,15 @@ edgeR_robust.pfun <-
 
 
 limma_voom.pfun <-
-  function(counts, group, design = NULL) 
+  function(counts, group, design = NULL, spikeins) 
   {   
-    ## limma voom pipeline ##
-    library(limma)
+    if(!is.null(spikeins)){
+      counts1 <- rbind(spikeins, counts)
+      set <- RUVg(as.matrix(counts1), 1:nrow(spikeins), k=1)
+      Phenod <- data.frame(x = group, weight = set$W)
+      rownames(Phenod) <- colnames(counts)
+      design <- model.matrix(~x + W_1, data = Phenod)
+    }
     nf <- calcNormFactors(counts)
     y <- voom(counts, design, plot=FALSE, lib.size = colSums(counts)*nf)
     fit <- lmFit(y, design)
@@ -187,12 +224,12 @@ limma_voom.pfun <-
       pval = TestStat$P.Value
     )
     colnames(TestStat) <- c('AveExpr',  'logFC', 'p value')
-    list(
+    return(list(
       RawCount = counts,
       NormCount = as.matrix(2^(y$E)),
       Dispersion = c(),
       TestStat = TestStat
-    )
+    ))
   }
 
 scde.pfun <- function(counts, design, cores = 4){
@@ -226,6 +263,68 @@ scde.pfun <- function(counts, design, cores = 4){
     TestStat = TestStat
   )
 }
+
+Brennecke_pfun <- function(counts, spikeins){
+  sfBio <- estimateSizeFactorsForMatrix(counts)
+  nCountsBio <- t(t(counts)/sfBio)
+  meansBio <- rowMeans( nCountsBio )
+  varsBio <- rowVars( nCountsBio )
+  cv2Bio <- varsBio / meansBio^2
+  if(is.null(spikeins)){
+    return(list(
+      normCounts <- nCountsBio,
+      CV <- cv2Bio,
+      DE_id <- order(cv2Bio, decreasing = T)[1:100]
+    ))
+  }
+  sfTec <- estimateSizeFactorsForMatrix(spikeins) 
+  nCountsTec <- t(t(spikeins)/sfTec)
+  
+  # Estimation of gene-specific expression rates
+  meansTec <- rowMeans( nCountsTec )
+  varsTec <- rowVars( nCountsTec )
+  cv2Tec <- varsTec / meansTec^2
+
+  
+  # HVG detection
+  minMeanForFit <- unname( quantile( meansTec[ which( cv2Tec > .3 ) ], .95 ) )
+  useForFit <- meansTec >= minMeanForFit
+  fit <- glmgam.fit( cbind( a0 = 1, a1tilde = 1/meansTec[useForFit] ),cv2Tec[useForFit] )
+  xi <- mean( 1 / sfTec )
+  a0 <- unname( fit$coefficients["a0"] )
+  a1 <- unname( fit$coefficients["a1tilde"] - xi )
+  psia1theta <- mean( 1 / sfBio ) + a1 * mean( sfTec / sfBio )
+  minBiolDisp <- .5^2
+  m <- ncol(counts)
+  cv2th <- a0 + minBiolDisp + a0 * minBiolDisp
+  testDenom <- ( meansBio * psia1theta + meansBio^2 * cv2th ) / ( 1 + cv2th/m )
+  p <- 1 - pchisq( varsBio * (m-1) / testDenom, m-1 )
+  padj <- p.adjust( p, "BH" )
+  sig <- padj < .1
+  sig[is.na(sig)] <- FALSE
+  HVG_Bren_id=which(sig)
+  return(list(
+    normCounts <- nCountsBio,
+    CV <- cv2Bio,
+    DE_id <- HVG_Bren_id
+  ))
+}
+
+par(mgp=c(6,2,0)); par(mar=c(8,10,4,0.5)); par(mfrow=c(1,3))
+plot(MCMC_Summary, Param = "mu", Param2 = "delta", main = "(a)", log="xy", cex.main = 2, cex.lab = 2, cex.axis = 1.5, col = 8, cex = 1.5)
+points(DetectHVG$Table[which(DetectHVG$Table[,5]>0.7925),2],DetectHVG$Table[which(DetectHVG$Table[,5]>0.7925),3],cex=1.5,pch=16,col="red")
+points(DetectLVG$Table[which(DetectLVG$Table[,5]>0.7650),2],DetectLVG$Table[which(DetectLVG$Table[,5]>0.7650),3],cex=1.5,pch=16,col="blue")
+legend('topright',c("HVG (BASiCS)","LVG (BASiCS)"),col=c("red","blue"),pch=16,cex=2,bty="n")
+
+plot(MCMC_Summary, Param = "mu", Param2 = "delta", main = "(b)", log="xy", cex.main = 2, cex.lab = 2, cex.axis = 1.5, col = 8, cex = 1.5)
+points(displaySummaryMu(MCMC_Summary)[1:q.bio,1][HVG_Islam_id],displaySummaryDelta(MCMC_Summary)[1:q.bio,1][HVG_Islam_id],cex=1.5,pch=16,col="red")
+legend('topright',c("HVG (Islam et al)"),col=c("red"),pch=16,cex=2,bty="n")
+
+plot(MCMC_Summary, Param = "mu", Param2 = "delta", main = "(c)", log="xy", cex.main = 2, cex.lab = 2, cex.axis = 1.5, col = 8, cex = 1.5)
+points(displaySummaryMu(MCMC_Summary)[1:q.bio,1][HVG_Bren_id],displaySummaryDelta(MCMC_Summary)[1:q.bio,1][HVG_Bren_id],cex=1.5,pch=16,col="red")
+legend('topright',c("HVG (Brennecke et al)"),col=c("red"),pch=16,cex=2,bty="n")
+
+
 
 renderChart3 <- function(expr, env = parent.frame(), quoted = FALSE) {
   func <- shiny::exprToFunction(expr, env, quoted)
